14864062e14fb78f21051c18da5ccb1dfc4b8780
Vincent Pauley <vinniepauley@gmail.com>
2023-02-15 20:49:40 -0600

    navigate between multiple scenes

---
+++ Assets/HomeControler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Something is happening now.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void enterPlayScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerSignup");
+    }
+}
